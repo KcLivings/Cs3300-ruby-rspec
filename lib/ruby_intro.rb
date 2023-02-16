db6f5e5a249c7c7466fac00930591771bac06ba7
KcLivings <clivings@uccs.edu>
2023-02-16 18:58:51 +0000

    more comments on code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,20 +3,24 @@
 # Part 1
 
 def sum(arr)
+  #create a sum variable
   sum_is = 0
+  #check length and dont sum if length is 0
   if arr.length > 0
+    #iterate through array, summing each as you go along
     arr.each {|x| sum_is += x }
   end  
   sum_is
 end
 
 def max_2_sum(array)
+  #check conditions
   if array.length == 0
     return 0
   elsif array.length == 1
     return array[0]
   else
-    # Sort the array in descending order and sum the first two elements
+    #if all met, Sort the array in descending order and sum the first two elements
     sorted_array = array.sort.reverse
     return sorted_array[0] + sorted_array[1]
   end
@@ -24,19 +28,20 @@ end
 
 
 def sum_to_n?(array, n)
-  # Create a hash to store each element and its index in the array
+  # create a hash to store each element and its index in the array
   hash = Hash.new
   
-  # Iterate through the array and check if each element's complement (i.e., the difference between the target sum and the element) exists in the hash
+  #iterate through the array and get compliment from n
   array.each_with_index do |element, index|
     complement = n - element
+    #check if the element's complement exists in the hash
     if hash.key?(complement)
       return true
     end
     hash[element] = index
   end
   
-  # If no pair of elements sums to n, return false
+  #If no pair of elements sums to n, return false
   return false
 end
 
@@ -44,12 +49,13 @@ end
 # Part 2
 
 def hello(name)
+  #adde name to hello
   return "Hello, " + name
 end
 
 def starts_with_consonant?(s)
-  # Check if the first character of the string is a letter
-  if s =~ /^[a-zA-Z]/
+  #check if the first character of the string is a letter
+  if s =~ /^[a-zA-Z]/ #regular expressionsssssssssss make this easy
     # Check if the first letter is a consonant (any letter other than A, E, I, O, U)
     return s =~ /^[^aeiou]/i
   else
@@ -58,9 +64,9 @@ def starts_with_consonant?(s)
 end
 
 def binary_multiple_of_4?(s)
-  # Check if the string represents a valid binary number
+  #Check if the string represents a valid binary number
   if s =~ /^[01]+$/
-    # Convert the binary string to an integer and check if it is divisible by 4
+    # convert the binary string to an integer and check if it is divisible by 4
     return s.to_i(2) % 4 == 0
   else
     return false
@@ -73,7 +79,7 @@ class BookInStock
   #create attributes for isbn and price
   attr_accessor :isbn, :price
   
-  #create constructor that takes isbn and price
+  # Create constructor that takes isbn and price
   def initialize(isbn, price)
     #if isbn or price dont work, error out
     if isbn.empty? || price <= 0
@@ -83,7 +89,7 @@ class BookInStock
     @price = price
   end
   
-  #convert the price string to a price that is accurate
+  # convert the price string to a price that is accurate
   def price_as_string
     sprintf("$%.2f", @price)
   end
